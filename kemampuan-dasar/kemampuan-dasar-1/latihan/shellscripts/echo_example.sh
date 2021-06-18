84e8d0f49e2c23088a974dc9917b83b12a728056
dabgetupp <64738209+dabgetupp@users.noreply.github.com>
2021-06-18 13:40:16 +0700

    Create echo_example.sh

---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/echo_example.sh
@@ -0,0 +1,7 @@
+Macbooks-MacBook-Air:~ Prastowoteguh$ echo_example.sh
+-bash: echo_example.sh: command not found
+Macbooks-MacBook-Air:~ Prastowoteguh$ nano echo_example.sh
+Macbooks-MacBook-Air:~ Prastowoteguh$ bash echo_example.sh
+Praxis academy mobile developer
+Praxis academy mobile developer
+Removing 	 backlash 	 characters
