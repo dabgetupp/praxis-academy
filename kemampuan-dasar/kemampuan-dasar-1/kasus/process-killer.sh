7cfb50bb765140b4482056d2703a4b34ad9d4ba1
dabgetupp <64738209+dabgetupp@users.noreply.github.com>
2021-06-19 22:08:49 +0700

    Create process-killer.sh

---
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/process-killer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter Your Program Name"
+read name
+program=`/bin/ps.exe -W | grep ${name}.exe`
+if [[ -n $program ]]; then
+PID=`echo $program | awk '{print $1}'`
+echo "Kill Program"
+echo ${program}
+/bin/kill.exe -f $PID
+fi
